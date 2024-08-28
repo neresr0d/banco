995f7b260cce53940c3d55cd377a528a6c1be865
neresr0d <rodrigoneres030@gmail.com>
2024-08-28 16:50:45 -0300

    adicionado VARCHAR AO telefone

--- sql/banco.sql
+++ sql/banco.sql
@@ -15,10 +15,12 @@ CREATE TABLE livros (
 CREATE TABLE usuario (
     id_usuario INT PRIMARY KEY AUTO_INCREMENT,
     nome_usuario VARCHAR(100) NOT NULL,
-    telefone VARCHAR()
+    telefone VARCHAR(20)
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(100) NOT NULL
     )
+
+    INSERT INTO usuario (nome_usuario, telefone, email, senha) VALUES ('rodrigo', 30421881, 'rodrigommm@gmail.com', "")
 
     CREATE TABLE categoria (
         id_categoria INT AUTO_INCREMENT PRIMARY KEY,
